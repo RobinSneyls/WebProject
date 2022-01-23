88cf24429921aa9aeac912b269bd9d2ca5ddf7e7
Андрей Косматов <52936446+RobinSneyls@users.noreply.github.com>
2022-01-23 16:56:10 +0300

    Update Modal.js

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -33,7 +33,7 @@ const Modal = ({ active, setActive, children }) => {
           className="Modal__close"
           onClick={() => {
             history.push('/');
-            setActive(false);
+            setActive(true);
           }}
         >
           ×
